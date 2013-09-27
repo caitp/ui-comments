88e6211af5d275785985d7e80693aac09aa09dad
Caitlin Potter <snowball@defpixel.com>
2013-09-26 20:35:39 -0400

    gh-pages v0.1.0-SNAPSHOT

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,37 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ui.comments.commentsConfig",
+      "shortName": "commentsConfig",
+      "type": "service",
+      "keywords": "$compile $element _when acceptable accessed actually all allows an and angular anywhere api appended application are array as at available be because being bind block_ blocks body by cached chainable checks child collections comment commentcontroller comments commentsconfig commenttemplate config configuration containertemplate containing controller current currently custom customization default depends detecting directive each either ensure entry events example except extend for function further generates given however html if in includes infinite inject injected inline instantiated instead intended into is it item its key keys known later loop machinery manipulate manually may method module must my mycontroller name named necessary ng-template no not notation object of offers on only or orderby ordering outside performed performs permits point presently programmatically properties property provide provider provides purpose registered release result rule safety scope service set setting should simply single so some specified state such support template templates that the there these this to type types ui url used valid validation value values whether which will wise with write written yet"
+    },
+    {
+      "section": "api",
+      "id": "ui.comments.directive:comment",
+      "shortName": "comment",
+      "type": "directive",
+      "keywords": "$compile access alt an and angularjs api automatically avatarurl building by cause class collection comment comment-avatar comment-body comment-data comment-date comment-header comment-username commentcontroller comments commentsconfig commenttemplate containing data date directive directives div do ea each eat element example exposed expressions following for http in infinite instantiated is it like look loop lot may memory might model name ng-bind ng-href ng-src of org primarily profileurl properties scope so specific specified template templates text the this through timeago title to ui use used username when will"
+    },
+    {
+      "section": "api",
+      "id": "ui.comments.directive:comments",
+      "shortName": "comments",
+      "type": "directive",
+      "keywords": "_self _self_ all an and api be by child child-container class collection comment comment-data commentdata commentdata_ comments commentsconfig contain container containertemplate containing data default directive div ea example expose exposed following for from in injected into is isolate it like look may might model ng-if ng-repeat of order-by orderby override parent produces properties replaced scope self shared should simple so status templates that the this transcluded ui value very with"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "ui-comments-0.0.4.js"
+  ]
+};
