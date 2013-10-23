d16cb22256ea2b5f017e66b34773585564701100
Caitlin Potter <snowball@defpixel.com>
2013-10-22 21:21:13 -0400

    gh-pages v0.1.0

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -8,7 +8,7 @@ NG_DOCS={
       "id": "ui.comments.commentsConfig",
       "shortName": "commentsConfig",
       "type": "service",
-      "keywords": "$compile $element _when acceptable accessed actually all allows an and angular anywhere api appended application are array as at available be because being bind block_ blocks body by cached chainable checks child collections comment commentcontroller comments commentsconfig commenttemplate config configuration containertemplate containing controller current currently custom customization default depends detecting directive each either ensure entry events example except extend for function further generates given however html if in includes infinite inject injected inline instantiated instead intended into is it item its key keys known later loop machinery manipulate manually may method module must my mycontroller name named necessary ng-template no not notation object of offers on only or orderby ordering outside performed performs permits point presently programmatically properties property provide provider provides purpose registered release result rule safety scope service set setting should simply single so some specified state such support template templates that the there these this to type types ui url used valid validation value values whether which will wise with write written yet"
+      "keywords": "$compile $element _when acceptable accessed actually all allows an and angular anywhere api appended application are array as at available be because being bind block_ blocks body by cached chainable checks child collections comment commentcontroller comments commentsconfig commenttemplate config configuration containertemplate containing controller current currently custom default depends detecting directive each either ensure events example except extend for function further generates given however html if in includes infinite inject injected inline instantiated instead intended into is it item its key keys known later loop machinery manipulate manually may method module must my mycontroller name named necessary ng-template no not notation object of offers on only or orderby ordering outside performed performs permits presently programmatically properties property provide provider provides purpose registered release result rule safety scope service set setting should simply single so some specified state such support template templates that the there these this to type types ui url used valid validation value values whether which will wise with write written yet"
     },
     {
       "section": "api",
@@ -23,6 +23,13 @@ NG_DOCS={
       "shortName": "comments",
       "type": "directive",
       "keywords": "_comments_ _status_ an and api be by child child-container class collection comment comment-data comments commentsconfig contain container containertemplate containing data default directive div ea example expose following for from in injected into is isolate it like look may might model ng-if ng-repeat of order-by orderby override parent produces replaced scope shared should simple so status templates that the this transcluded ui value very with"
+    },
+    {
+      "section": "api",
+      "id": "ui.comments.directive:commentsTransclude",
+      "shortName": "commentsTransclude",
+      "type": "directive",
+      "keywords": "allows alt an and any api appended are available avatarurl be behaviour by categories child class collection comment comment-avatar comment-body comment-date comment-header comment-username comments comments-transclude completely date default directive div ea element end example following helper if ignored in inserted instead is it like literal look merely might name names ng-bind ng-href ng-src not of or profileurl replaced so specify template templates text the then they this timeago title to transclusion ui used user username when where which wish"
     }
   ],
   "apis": {
@@ -32,6 +39,6 @@ NG_DOCS={
   "startPage": "/api",
   "scripts": [
     "angular.min.js",
-    "ui-comments-0.0.10.js"
+    "ui-comments-0.1.0.js"
   ]
 };
--- js/app.js
+++ js/app.js
@@ -3,19 +3,24 @@ angular.module("views/comment.html", []).run(["$templateCache", function($templa
     "<div class=\"container comment\">\n" +
     "  <div class=\"page-header\">\n" +
     "    <div class=\"comment-header\">\n" +
+    "      <button ng-if=\"comment.children\" class=\"icon-2x clear-square glyphicon\" " +
+    "              ng-class=\"{'icon-minus-sign-alt': !collapsed, 'icon-plus-sign-alt': collapsed}\" " + 
+    "              title=\"toggle children\" ng-click=\"collapse()\"></button>" +
+    "      <button ng-if=\"!comment.children\" class=\"icon-2x clear-square\"></button>" +
     "      <h4 class=\"comment-user\">\n" +
-    "        <button ng-if=\"comment.children\" class=\"icon-2x clear-square glyphicon\" ng-class=\"{'icon-minus-sign-alt': !collapsed, 'icon-plus-sign-alt': collapsed}\" title=\"toggle children\" ng-click=\"collapse()\"></button>" +
-    "        <button ng-if=\"!comment.children\" class=\"icon-2x clear-square\"></button>" +
     "        <span class=\"comment-username\" ng-if=\"!comment.profileUrl\">{{comment.name}}</span>\n" +
-    "        <a class=\"comment-username\" ng-if=\"comment.profileUrl\" ng-href=\"{{comment.profileUrl}}\" title=\"{{comment.name}}\">{{comment.name}}</a>\n" +
-    "        <small class=\"comment-date\" ng-if=\"comment.date\" title=\"{{comment.date | calendar}}\">{{comment.date | timeago}}</small>\n" +
-    "       </h4>\n" +
-    "      <img class=\"comment-avatar\" ng-if=\"comment.avatarUrl\" ng-src=\"{{comment.avatarUrl}}\" alt=\"{{comment.name}}\" />\n" +
+    "        <a class=\"comment-username\" ng-if=\"comment.profileUrl\" ng-href=\"{{comment.profileUrl}}\" " +
+    "           title=\"{{comment.name}}\">{{comment.name}}</a>\n" +
+    "        <small class=\"comment-date\" ng-if=\"comment.date\" title=\"{{comment.date | calendar}}\">" +
+    "          {{comment.date | timeago}}" +
+    "        </small>\n" +
+    "      </h4>\n" +
+    "      <img class=\"comment-avatar\" ng-if=\"comment.avatarUrl\" ng-src=\"{{comment.avatarUrl}}\" " +
+    "           alt=\"{{comment.name}}\" />\n" +
     "    </div>\n" +
     "  </div>\n" +
-    "  <div class=\"row\">\n" +
-    "    <div class=\"container comment-body\" ng-bind=\"comment.text\"></div>\n" +
-    "  </div>\n" +
+    "  <div class=\"comment-body\" ng-bind=\"comment.text\"></div>" +
+    "  <div comments-transclude></div>" +
     "</div>");
 }]);
 
@@ -62,9 +67,9 @@ angular.module('commentsDemo', ['views/comments.html', 'views/comment.html', 'ui
       profileUrl: 'https://github.com/caitp',
       text: 'UI-Comments is designed to simplify the process of creating comment systems similar to Reddit, Imgur or Discuss in AngularJS.',
       children: [{
-        name: '@caitp',
+        name: '@bizarro-caitp',
         date: new Date(),
-        profileUrl: 'https://github.com/caitp',
+        profileUrl: 'https://github.com/bizarro-caitp',
         text: 'We support nested comments, in a very simple fashion. It\'s great!',
         children: [{
           name: '@caitp',
@@ -72,15 +77,52 @@ angular.module('commentsDemo', ['views/comments.html', 'views/comment.html', 'ui
           profileUrl: 'https://github.com/caitp',
           text: 'These nested comments can descend arbitrarily deep, into many levels. This can be used to reflect a long and detailed conversation about typical folly which occurs in comments',
           children: [{
+            name: '@bizarro-caitp',
+            date: new Date(),
+            profileUrl: 'https://github.com/bizarro-caitp',
+            text: 'Having deep conversations on the internet can be used to drive and derive data about important topics, from marketing demographic information to political affiliation and even sexual orientation if you care to find out about that. Isn\'t that exciting?'
+          }]
+        },{
+          name: '@bizarro-caitp',
+          date: new Date(),
+          profileUrl: 'https://github.com/bizarro-caitp',
+          text: 'Is it REALLY all that wonderful? People tend to populate comments with innane nonsense that ought to get them hellbanned!',
+          comments: [{
             name: '@caitp',
             date: new Date(),
             profileUrl: 'https://github.com/caitp',
-            text: 'Having deep conversations on the internet can be used to drive and derive data about important topics, from marketing demographic information to political affiliation and even sexual orientation if you care to find out about that. Isn\'t that exciting?'
+            text: 'Oh whatever lady, whatever'
           }]
         }]
       }]
-    }
-  ]
+    }, {
+      name: '@caitp',
+      date: new Date(),
+      profileUrl: 'https://github.com/caitp',
+      text: 'We can have multiple threads of comments at a given moment...',
+    }, {
+      name: '@bizarro-caitp',
+      date: new Date(),
+      profileUrl: 'https://github.com/bizarro-caitp',
+      text: 'We can do other fancy things too, maybe...',
+      children: [{
+        name: '@caitp',
+        date: new Date(),
+        profileUrl: 'https://github.com/caitp',
+        text: '...other fancy things, you say?',
+      }, {
+        name: '@caitp',
+        date: new Date(),
+        profileUrl: 'https://github.com/caitp',
+        text: 'suddenly I\'m all curious, what else can we do...',
+        children: [{
+          name: '@bizarro-caitp',
+          date: new Date(),
+          profileUrl: 'https://github.com/bizarro-caitp',
+          text: 'Oh, you\'ll see...',
+        }]
+      }]
+    }]
 })
 
 .filter('timeago', function() {
