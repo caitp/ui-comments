3e000d702e3f8faf8ab5797ebfcdf8101a5c97e5
Ariel Mashraki <ariel@codeoasis.com>
2014-06-10 08:58:50 +0300

    feat(commeter): create base assets

---
+++ misc/demo/js/directive/typeahead.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ariel Mashraki
+ */
+
+function typeheadDirective(){
+  return {
+    restrict: 'E',
+    link: function(scope, elm, att) {
+      scope.handleSelection = function(val){
+        scope.child.name = val;
+        scope.selected = true;
+      };
+    },
+    template: '<ul class="nav nav-pills nav-stacked" ng-hide="selected">' +
+      '<li ng-repeat="item in items | filter:item track by $index" ng-click="handleSelection(item.login)" class="pointer">' +
+      '<a>{{ item.login }}</a></li>' +
+      '</ul>'
+  };
+}
