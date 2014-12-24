fa0413a5c4a8a37a0aa92a545fc29de54ccf8617
Caitlin Potter <caitpotter88@gmail.com>
2014-12-23 21:58:17 -0500

    chore(tests): migrate to Jasmine 2.0

--- misc/test/helpers.js
+++ misc/test/helpers.js
@@ -1,13 +1,18 @@
 // jasmine matcher for expecting an element to have a css class
 // https://github.com/angular/angular.js/blob/master/test/matchers.js
 beforeEach(function() {
-  this.addMatchers({
+  jasmine.addMatchers({
     toHaveClass: function(cls) {
-      this.message = function() {
-        return "Expected '" + angular.mock.dump(this.actual) + "' to have class '" + cls + "'.";
+      return {
+        compare: function(actual, expected) {
+          return {
+            message: function() {
+              return "Expected '" + angular.mock.dump(actual) + "' to have class '" + expected + "'.";
+            },
+            pass: angular.element(actual).hasClass(expected)
+          };
+        }
       };
-
-      return this.actual.hasClass(cls);
     }
   });
 });
--- src/test/comments.spec.js
+++ src/test/comments.spec.js
@@ -24,7 +24,7 @@ describe('ui.comments', function() {
     ], function(m) {
       module(m);
     });
-  
+
     inject(function(_$rootScope_, _$document_, _$compile_) {
       $scope = _$rootScope_;
       $document = _$document_;
@@ -49,30 +49,30 @@ describe('ui.comments', function() {
 
 
       it('adds comment to DOM when comment model grows', function() {
-        expect(comments.children().length).toEqual(0);
+        expect(comments.children().length).toBe(0);
         $scope.comments.push({});
         $scope.$digest();
-        expect(comments.children().length).toEqual(1);
+        expect(comments.children().length).toBe(1);
       });
 
 
       it('removes comment from DOM when comment model shrinks', function() {
         $scope.comments.push({});
         $scope.$digest();
-        expect(comments.children().length).toEqual(1);
+        expect(comments.children().length).toBe(1);
         $scope.comments.pop();
         $scope.$digest();
-        expect(comments.children().length).toEqual(0);
+        expect(comments.children().length).toBe(0);
       });
 
 
       it('changes comment data when comment model changes', function() {
         $scope.comments.push({text: 'Test Comment'});
         $scope.$digest();
-        expect(comments.find('.comment-body > div').text()).toEqual("Test Comment");
+        expect(comments.find('.comment-body > div').text()).toBe("Test Comment");
         $scope.comments[0].text = 'Changed Comment';
         $scope.$digest();
-        expect(comments.find('.comment-body > div').text()).toEqual("Changed Comment");
+        expect(comments.find('.comment-body > div').text()).toBe("Changed Comment");
       });
 
 
@@ -80,10 +80,10 @@ describe('ui.comments', function() {
         $scope.comments.push({text: '123'});
         $scope.comments.push({text: 'ABC'});
         $scope.$digest();
-        expect(comments.find('.comment-body > div').first().text()).toEqual('123');
+        expect(comments.find('.comment-body > div').first().text()).toBe('123');
         $scope.comments.reverse();
         $scope.$digest();
-        expect(comments.find('.comment-body > div').first().text()).toEqual('ABC');
+        expect(comments.find('.comment-body > div').first().text()).toBe('ABC');
       });
     });
 
@@ -103,36 +103,36 @@ describe('ui.comments', function() {
 
 
       it('adds comment to DOM when child comment model grows', function() {
-        expect(comments.find('.child-comment').length).toEqual(2);
+        expect(comments.find('.child-comment').length).toBe(2);
         $scope.comments[0].children.push({text: 'Test'});
         $scope.$digest();
-        expect(comments.find('.child-comment').length).toEqual(3);
+        expect(comments.find('.child-comment').length).toBe(3);
       });
 
 
       it('removes comment from DOM when child comment model shrinks', function() {
-        expect(comments.find('.child-comment').length).toEqual(2);
+        expect(comments.find('.child-comment').length).toBe(2);
         $scope.comments[0].children.pop();
         $scope.$digest();
-        expect(comments.find('.child-comment').length).toEqual(1);
+        expect(comments.find('.child-comment').length).toBe(1);
       });
 
 
       it('changes comment data when child comment model changes', function() {
         var first = comments.find('.child-comment > .comment-body > div').first();
-        expect(first.text()).toEqual("First child");
+        expect(first.text()).toBe("First child");
         $scope.comments[0].children[0].text = 'Changed Comment';
         $scope.$digest();
-        expect(first.text()).toEqual("Changed Comment");
+        expect(first.text()).toBe("Changed Comment");
       });
 
 
       it('re-orders comments in DOM when child comment model is re-ordered', function() {
         var children = comments.find('.child-comment');
-        expect(children.find('.comment-body > div').first().text()).toEqual('First child');
+        expect(children.find('.comment-body > div').first().text()).toBe('First child');
         $scope.comments[0].children.reverse();
         $scope.$digest();
-        expect(children.find('.comment-body > div').first().text()).toEqual('Second child');
+        expect(children.find('.comment-body > div').first().text()).toBe('Second child');
       });
     });
 
@@ -154,7 +154,7 @@ describe('ui.comments', function() {
         comments = $compile('<comments comment-depth-limit="{{depth}}" ' +
                             'comment-data="comments"></comments>')($scope);
         $scope.$digest();
-        expect(comments.find('.comment').length).toEqual(1);
+        expect(comments.find('.comment').length).toBe(1);
       });
     })
   });
@@ -176,8 +176,9 @@ describe('ui.comments', function() {
       $scope.comments[0].children = [{}];
       $scope.$digest();
       expect(callback).toHaveBeenCalled();
-			expect(callback.mostRecentCall.args[0][0]).toEqual(comments.find('.comments').first()[0]);
-			expect(callback.mostRecentCall.args[0].children().length).toEqual(1);
+			expect(callback.calls.mostRecent().args[0][0]).
+          toBe(comments.find('.comments').first()[0]);
+			expect(callback.calls.mostRecent().args[0].children().length).toBe(1);
     });
 
 
@@ -191,9 +192,11 @@ describe('ui.comments', function() {
       $scope.comments[0].children = [];
       $scope.$digest();
       expect(callback).toHaveBeenCalled();
-			expect(comments.find('.comment .comments-transclude').first().length).toEqual(1);
-			expect(callback.mostRecentCall.args[0]).toHaveClass('comments-transclude');
-			expect(callback.mostRecentCall.args[0].children().length).toEqual(0);
+			expect(comments.find('.comment .comments-transclude').first().length).toBe(1);
+			expect(callback.calls.mostRecent().args[0]).
+          toHaveClass('comments-transclude');
+			expect(callback.calls.mostRecent().args[0].children().length).
+          toBe(0);
     });
 
 
@@ -214,7 +217,7 @@ describe('ui.comments', function() {
 
   describe('custom comment controller', function() {
     var commentsConfig;
-    beforeEach(function() {    
+    beforeEach(function() {
       inject(function(_commentsConfig_) {
         commentsConfig = _commentsConfig_;
       });
@@ -225,12 +228,12 @@ describe('ui.comments', function() {
       commentsConfig.commentController = 'TestCtrl1';
       comments = $compile('<comments comment-data="comments"></comments>')($scope);
       $scope.$digest();
-      expect(firstCtrl().controllerName).toEqual('TestCtrl1');
+      expect(firstCtrl().controllerName).toBe('TestCtrl1');
 
       commentsConfig.commentController = 'TestCtrl2';
       comments = $compile('<comments comment-data="comments"></comments>')($scope);
       $scope.$digest();
-      expect(firstCtrl().controllerName).toEqual('TestCtrl2');
+      expect(firstCtrl().controllerName).toBe('TestCtrl2');
     });
 
 
@@ -240,7 +243,7 @@ describe('ui.comments', function() {
       };
       comments = $compile('<comments comment-data="comments"></comments>')($scope);
       $scope.$digest();
-      expect(firstCtrl().controllerName).toEqual('TestCtrl3');
+      expect(firstCtrl().controllerName).toBe('TestCtrl3');
     });
 
 
@@ -250,7 +253,7 @@ describe('ui.comments', function() {
       }];
       comments = $compile('<comments comment-data="comments"></comments>')($scope);
       $scope.$digest();
-      expect(firstCtrl().controllerName).toEqual('TestCtrl4');
+      expect(firstCtrl().controllerName).toBe('TestCtrl4');
     });
 
 
@@ -258,7 +261,7 @@ describe('ui.comments', function() {
       commentsConfig.commentController = 'TestCtrl2';
       comments = $compile('<comments comment-data="comments"></comments>')($scope);
       $scope.$digest();
-      expect(firstComment()[0]).toEqual(firstCtrl().$element[0]);
+      expect(firstComment()[0]).toBe(firstCtrl().$element[0]);
     });
   });
 });
